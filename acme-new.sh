a1531dd720be5836c498168f0c65b82134e4ae42
Derrity <derrity0731@gmail.com>
2022-07-11 09:06:26 +0800

    Update acme-new.sh

--- acme-new.sh
+++ acme-new.sh
@@ -14,10 +14,4 @@ git clone https://github.com/acmesh-official/acme.sh.git
 ./acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
 echo -e "Your Key File Is In /root/private.key\n"
 echo -e "Your Fullchain File Is In /root/cert.crt\n"
-read -p "Do you want to download bbr?(y/n)" choose
-if [$choose="y"];then
-	echo -e "Starting downloading bbr.\n"
-	wget -N --no-check-certificate "https://gist.github.com/zeruns/a0ec603f20d1b86de6a774a8ba27588f/raw/4f9957ae23f5efb2bb7c57a198ae2cffebfb1c56/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
-if [$choose="n"];then
-	echo "Thanks for use."
-	exit n
+
